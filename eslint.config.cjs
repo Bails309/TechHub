8b60962ac4d1cba7ca41be7dc362e07724b5293f
sa_baileym <sa_baileym@capita-ics.co.uk>
2026-02-24 22:52:30 +0000

    eslint config update

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,10 +1,6 @@
 const { resolve } = require('path');
 
 module.exports = [
-  // Next.js ESLint plugin as a top-level config so Next's build
-  // can detect and apply Next-specific lint rules when running
-  // `next build`.
-  require('@next/eslint-plugin-next'),
   {
     // Language / parser settings to support TypeScript + JSX
     languageOptions: {
